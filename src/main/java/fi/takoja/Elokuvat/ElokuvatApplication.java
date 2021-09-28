9cff98c7bd1719b72f521ccd43049af23a28d262
Niko Takoja <niko@takoja.fi>
2021-09-28 19:53:53 +0300

    v0

---
+++ src/main/java/fi/takoja/Elokuvat/ElokuvatApplication.java
@@ -0,0 +1,13 @@
+package fi.takoja.Elokuvat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElokuvatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElokuvatApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fi/takoja/Elokuvat/ElokuvatApplicationTests.java
@@ -0,0 +1,13 @@
+package fi.takoja.Elokuvat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElokuvatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
